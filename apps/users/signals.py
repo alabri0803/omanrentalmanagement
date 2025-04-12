21a1299e5259561acd699132e986d7943993a686
alabri0803 <rashid92759346@gmail.com>
2025-04-12 11:45:34 +0000

    sig users

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -1,21 +1,22 @@
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.translation import gettext_lazy as _
+
 from .models import User
 
+
 @receiver(post_save, sender=User)
-def send_welcome_email(sender, instance, created, **kwargs):
-    if created and instance.email:
-        subject = _('مرحباً بك في نظام إدارة الإيجارات')
-        message = render_to_string('users/email/welcome.html', {
-            'user': instance,
-        })
-        send_mail(
-            subject,
-            message,
-            'noreply@rental-system.com',
-            [instance.email],
-            fail_silently=False,
-        )
+def handle_user_creation(sender, instance, created, **kwargs):
+    if created:
+        if instance.email:
+            send_verification_email(instance)
+
+    if hasattr(instance, 'profile'):
+        return Profile.objects.create(user=instance)
+
+def send_verification_email(user):
+    subject = _('تفعيل حسابك في نظام إدارة الإيجارات')
+    message = render_to_string('emails/verify_account.html', {'user': user, 'verification_token': user.verification_token})
+    send_mail(subject, message, 'no-reply@rental-management.com', [user.email], fail_silently=False)
